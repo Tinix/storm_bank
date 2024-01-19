ed7b22c94ac0ac713d0cff04023a26a818e9a0ea
Daniel Tinivella <dtinivella@gmail.com>
2024-01-19 19:29:21 -0300

    add Argon2 password hashing algorithm for Elixir

--- lib/banaba_bank/users/user.ex
+++ lib/banaba_bank/users/user.ex
@@ -2,9 +2,14 @@ defmodule BanabaBank.Users.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Ecto.Changeset
+
+  @required_fields [:name, :email, :password, :cep]
+
   schema "users" do
     field :email, :string
     field :name, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
     field :cep, :string
 
@@ -13,7 +18,17 @@ defmodule BanabaBank.Users.User do
 
   def changeset(user \\ %__MODULE__{}, attrs) do
     user
-    |> cast(attrs, [:name, :email, :password_hash, :cep])
-    |> validate_required([:name, :email, :password_hash, :cep])
+    |> cast(attrs, @required_fields)
+    |> validate_required(@required_fields)
+    |> validate_length(:name, min: 3)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:cep, is: 8)
+    |> add_password_hash()
   end
+
+  defp add_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Argon2.add_hash(password))
+  end
+
+  defp add_password_hash(changeset), do: changeset
 end
